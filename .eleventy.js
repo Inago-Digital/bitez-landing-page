950e2126d3d28dcb81b8086855cf8be05c7e2d90
HangerThem <f.borisjuk@hangerthem.com>
2025-03-10 22:04:22 +0100

    Meta tags update

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets")
-  eleventyConfig.addPassthroughCopy("src/js")
   eleventyConfig.addPassthroughCopy("src/css")
+  eleventyConfig.addPassthroughCopy("src/js")
   eleventyConfig.addPassthroughCopy("CNAME")
+  eleventyConfig.addPassthroughCopy({
+    "src/favicon": "/",
+  })
 
   eleventyConfig.addWatchTarget("src/css/")
 
