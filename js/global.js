ee914ebebca633dd626ae342bddcd4b9efde526d
HangerThem <HangerThem@users.noreply.github.com>
2025-03-11 13:19:35 +0000

    deploy: c425605fef3737e7e34210c40c93b2463d0f4c2e

--- js/global.js
+++ js/global.js
@@ -33,6 +33,54 @@ document.addEventListener("DOMContentLoaded", function () {
     },
   }
 
+  const mobileMenuToggle = document.querySelector("[data-mobile-menu-toggle]")
+  const mobileMenu = document.querySelector("[data-mobile-menu]")
+  const navigation = document.querySelector("[data-navigation]")
+
+  const classes = ["bg-white", "shadow-xl"]
+
+  if (mobileMenuToggle && mobileMenu) {
+    let isMenuOpen = false
+
+    mobileMenuToggle.addEventListener("click", () => {
+      isMenuOpen = !isMenuOpen
+
+      if (isMenuOpen) {
+        mobileMenu.style.display = "flex"
+        navigation.classList.remove(...classes)
+        document.body.style.overflow = "hidden"
+
+        const bars = mobileMenuToggle.querySelectorAll("div")
+        bars[0].style.transform = "rotate(45deg) translate(7px, 7px)"
+        bars[1].style.opacity = "0"
+        bars[2].style.transform = "rotate(-45deg) translate(7px, -7px)"
+      } else {
+        mobileMenu.style.display = "none"
+        navigation.classList.add(...classes)
+        document.body.style.overflow = ""
+
+        const bars = mobileMenuToggle.querySelectorAll("div")
+        bars[0].style.transform = ""
+        bars[1].style.opacity = "1"
+        bars[2].style.transform = ""
+      }
+    })
+
+    const mobileMenuLinks = mobileMenu.querySelectorAll("a")
+    mobileMenuLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        mobileMenu.style.display = "none"
+        document.body.style.overflow = ""
+        isMenuOpen = false
+
+        const bars = mobileMenuToggle.querySelectorAll("div")
+        bars[0].style.transform = ""
+        bars[1].style.opacity = "1"
+        bars[2].style.transform = ""
+      })
+    })
+  }
+
   const faqQuestions = document.querySelectorAll("[data-faq-question]")
 
   faqQuestions.forEach((question) => {
