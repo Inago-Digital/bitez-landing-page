ed7cdbfc710976b52ea0c6d58fae65bb868e46a8
HangerThem <HangerThem@users.noreply.github.com>
2025-03-13 19:28:30 +0000

    deploy: e802a4b66741acc76cb534ee7d1d66f8e36463f7

--- js/global.js
+++ js/global.js
@@ -1,38 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  lucide.createIcons()
-
-  tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          primary: "#ee2554",
-          secondary: "#c6264b",
-          tertiary: "#262e49",
-          accent: {
-            DEFAULT: "#ffd958",
-            dark: "#ffd33f",
-          },
-          dark: "#1c1c1c",
-          light: "#e1e9ee",
-          gray: "#c7Ceda",
-          "light-gray": "#D0D7EF",
-          muted: "#ebF0f4",
-          white: "#ffffff",
-          black: "#000000",
-          "blue-gray": "#8b9ab3",
-          "light-green": "#4cc36c",
-          "dark-green": "#3aa25f",
-          "light-pink": "#f6d3db",
-          blue: "#117cf7",
-          purple: "#b16bed",
-        },
-        fontFamily: {
-          champ: ["Champ", "sans-serif"],
-        },
-      },
-    },
-  }
-
   const mobileMenuToggle = document.querySelector("[data-mobile-menu-toggle]")
   const mobileMenu = document.querySelector("[data-mobile-menu]")
   const navigation = document.querySelector("[data-navigation]")
