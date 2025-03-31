69385d9cf067e54624e52d98b255d1f6c4a5dfa1
HangerThem <HangerThem@users.noreply.github.com>
2025-03-31 16:55:44 +0000

    deploy: 9d5ebb6934d883b4acd47a8fd98f6fbe969ef031

---
+++ js/icons.js
@@ -0,0 +1,27 @@
+const getIcon = (name) => {
+  const path = `assets/icons/${name}.svg`
+  const request = new XMLHttpRequest()
+
+  request.open("GET", path, false)
+  request.send()
+
+  if (request.status === 200) {
+    return request.responseText
+  }
+
+  return null
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const icons = document.querySelectorAll("i[data-lucide]")
+
+  icons.forEach((icon) => {
+    const name = icon.getAttribute("data-lucide")
+    const svg = getIcon(name)
+
+    if (svg) {
+      const classes = icon.getAttribute("class")
+      icon.outerHTML = svg.replace("<svg", `<svg class="${classes}"`)
+    }
+  })
+})
